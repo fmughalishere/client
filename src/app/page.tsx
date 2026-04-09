233438639493aa142a9fc4b465fe8afadd6ba56c
fmughalishere <fizamuneer0101@gmail.com>
2026-04-09 17:20:40 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function HomePage() {
     <main className="min-h-screen bg-[#fcfcfc] pb-8 font-sans">
       <section className="px-0 pt-0 relative">
         <div className="bg-[#e2f2f5] rounded-b-[35px] pt-12 pb-12 px-6 flex flex-col items-center shadow-sm relative">
-        <div className="bg-white text-[#00004d] font-normal px-5 py-2 rounded-full tracking-wide text-[13px] absolute top-2 z-30 shadow-lg border border-bg-[#00004d]">
+        <div className="bg-white text-[#00004d] font-normal px-5 py-2 rounded-full tracking-wide text-[13px] absolute top-2 z-30 shadow-lg border border-[#00004d]">
         Real-Time Visitors: 300{visitorCount}
          </div>            
           <div className="text-center mb-1 mt-6">
