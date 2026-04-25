63498adfd595861fb3f0ab514b40bed697e98520
Fiza Muneer <fizamuneer0101@gmail.com>
2026-04-25 13:59:09 +0500

    Update page.tsx

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,4 +1,4 @@
-""use client";
+"use client";
 
 import React, { useState, useRef, ChangeEvent, FormEvent, useMemo, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
