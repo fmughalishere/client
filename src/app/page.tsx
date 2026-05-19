3c08a09681a1144ba61b38a2649a8dae4bcbcd6a
fmughalishere <fizamuneer0101@gmail.com>
2026-05-19 15:25:33 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function HomePage() {
   };
 
   return (
-    <main className="min-h-[70vh] bg-[#fcfcfc] font-sans">
+    <main className="min-h-[73vh] bg-[#fcfcfc] font-sans">
       <section className="px-0 pt-0 relative">
         <div className="bg-white rounded-b-[40px] pt-8 pb-12 px-6 flex flex-col items-center shadow-sm relative overflow-hidden">
           <div className="text-center mb-1 mt-0 relative z-10">
