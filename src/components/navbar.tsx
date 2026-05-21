c03115d289711122bfb95c14ec21ca09570b93e0
fmughalishere <fizamuneer0101@gmail.com>
2026-05-21 15:32:31 +0500

    Updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -135,7 +135,7 @@ const Navbar = () => {
         <div className="flex items-center justify-center gap-3 px-3">
           <Link
             href="/"
-            className="flex items-center justify-center gap-1 bg-[#e1eaed] rounded-[13px] px-3 h-[30px] shadow-md hover:shadow-lg transition"
+            className="flex items-center justify-center gap-1 bg-[#e1eaed] rounded-[8px] px-3 h-[25px] shadow-md hover:shadow-lg transition"
           >
             <AiFillHome size={20} style={{ color: "#5DBB63" }} />
             <span className="font-bold text-[12px] text-[#00004d]">
@@ -145,7 +145,7 @@ const Navbar = () => {
 
           <Link
             href={dashboardLink}
-            className="flex items-center justify-center gap-1 bg-[#e1eaed] rounded-[13px] px-3 h-[30px] shadow-md hover:shadow-lg transition"
+            className="flex items-center justify-center gap-1 bg-[#e1eaed] rounded-[8px] px-3 h-[25px] shadow-md hover:shadow-lg transition"
           >
             <FaUserGear size={20} style={{ color: "#5DBB63" }} />
             <span className="font-bold text-[12px] text-[#00004d] whitespace-nowrap">
@@ -155,7 +155,7 @@ const Navbar = () => {
 
           <button
             onClick={() => setShowInstallBox(true)}
-            className="flex items-center justify-center gap-1 bg-[#e1eaed] rounded-[13px] px-3 h-[30px] shadow-md hover:shadow-lg transition"
+            className="flex items-center justify-center gap-1 bg-[#e1eaed] rounded-[8px] px-3 h-[25px] shadow-md hover:shadow-lg transition"
           >
             <PlusCircle size={20} style={{ color: "#5DBB63" }} />
             <span className="font-bold text-[12px] text-[#00004d] whitespace-nowrap">
